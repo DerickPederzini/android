662ea39dabb69e3ec7556327af6656ef3918962a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 11:02:23 -0300

    placar yes

--- Projeto/app/src/main/java/com/DerickPederzini/placar/MainActivity.kt
+++ Projeto/app/src/main/java/com/DerickPederzini/placar/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.DerickPederzini.placar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var playerOneScore = 0;
+    private var playerTwoScore = 0;
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setUpExtras()
+        handleScore()
+    }
+
+    private fun setUpExtras(){
+        binding.tvPlayer1.text = intent.getStringExtra("PLAYER1")
+        binding.tvPlayer2.text = intent.getStringExtra("PLAYER2")
+    }
+
+    private fun handleScore(){
+        binding.btCasa.setOnClickListener{
+            playerOneScore++
+            binding.tvPlayer1Name.text = playerOneScore.toString()
+
+        }
+        binding.btVisitante.setOnClickListener{
+            playerTwoScore++
+            binding.tvPlayer2Name.text = playerTwoScore.toString()
+
         }
     }
 }
--- Projeto/app/src/main/java/com/DerickPederzini/placar/SplashActivity.kt
+++ Projeto/app/src/main/java/com/DerickPederzini/placar/SplashActivity.kt
@@ -1,20 +1,29 @@
 package com.DerickPederzini.placar
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.DerickPederzini.placar.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private val DUR_SPLASH_MILLIS = 2000L
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val nextScreen = Intent(this, PlayerActivity::class.java)
+            startActivity(nextScreen)
+            finish()
+        }, DUR_SPLASH_MILLIS)
+
     }
 }
